ab0883c237d349cae5f360dc3adee5940ad26309
jeffren10 <ijeffren1@gmail.com>
2024-03-08 11:39:14 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <div className="flex h-screen">
+        <div className="flex-1 flex flex-col bg-green-400">kleke</div>
+        <div className="flex-1 flex flex-col bg-green-200">kklerlwk</div>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
